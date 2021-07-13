d1bae89711840c4263b381a8e639306e8158021c
Tim Marchant <fromtimwithcode@gmail.com>
2021-07-13 16:21:02 -0500

    Completed task 2

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
 
-
+console.log(dogYears(5));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
