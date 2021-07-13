aaca1912e02a25d33ad87bfef60c61f7438c337a
Tim Marchant <fromtimwithcode@gmail.com>
2021-07-13 16:56:17 -0500

    Completed task 5a

--- index.js
+++ index.js
@@ -199,11 +199,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log(miles(14));
 
 //Task 5b - Feet to CM
 /*
